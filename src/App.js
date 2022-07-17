139659a340aa3b5ad18807de6f36c9706f174b35
SwimmingPotato <ptphong200i@gmail.com>
2022-07-17 08:23:52 +0700

    Add tracking input count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React, { useEffect, useState } from "react";
 import Product from "./Product";
+import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [inputCount, setInputCount] = useState([{ id: 0, count: 0 }]);
 
   useEffect(() => {
     async function getProducts() {
       const res = await fetch("https://fakestoreapi.com/products");
       const data = await res.json();
       setProducts(data);
-    };
+    }
 
     getProducts();
   }, []);
 
-  return <div className="App">
-    {console.log(products)}
-    {products.map((e) => {
-      return <Product key={e.id} title={e.title} description={e.description} image={e.image} price={e.price} />
-    })}
-  </div>
+  useEffect(() => {
+    const newCart = products.map((product) => {
+      return { product: product, itemCount: 0 };
+    });
+    setCart(newCart);
+  }, [products]);
+
+  useEffect(() => {
+    const newCount = products.map((product) => {
+      return { id: product.id, count: 1 };
+    });
+    setInputCount(newCount);
+    console.log(newCount);
+  }, [products]);
+
+  function handleValue(id) {
+    const item = inputCount.find((product) => product.id === id);
+    if (!item) {
+      return 1;
+    }
+    return item.count;
+  }
+
+  function handleChange(evt) {
+    const newCount = inputCount.map((e) => {
+      if (e.id === parseInt(evt.target.name)) {
+        return {
+          id: parseInt(evt.target.name),
+          count: parseInt(evt.target.value),
+        };
+      }
+      return e;
+    });
+    console.log(newCount);
+    setInputCount(newCount);
+  }
+
+  return (
+    <div className="App">
+      <div className="products-container">
+        {products.map((e) => {
+          return (
+            <div key={e.id}>
+              <Product
+                title={e.title}
+                description={e.description}
+                image={e.image}
+                price={e.price}
+              />
+              <label htmlFor="quantity">Qty</label>
+              <input
+                type="number"
+                min={1}
+                value={handleValue(e.id)}
+                name={e.id}
+                onChange={handleChange}
+              ></input>
+              <button>Add to cart</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
