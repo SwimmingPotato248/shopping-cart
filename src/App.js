f2c6ab8d978d8cd6a81cfb72f4523f79c6817423
SwimmingPotato <ptphong200i@gmail.com>
2022-08-23 16:55:45 +0700

    Refactor code and use context to organize

--- src/App.js
+++ src/App.js
@@ -1,98 +1,39 @@
-import React, { useEffect, useState } from "react";
-import Product from "./Product";
 import "./App.css";
+import Home from "./Home";
+import Shop from "./Shop";
+import ItemDetail from "./ItemDetail";
+import Cart from "./Cart";
+import { Routes, Route, Link } from "react-router-dom";
+import { useCart } from "./CartContext";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
-  const [inputCount, setInputCount] = useState([{ id: 0, count: 0 }]);
-
-  useEffect(() => {
-    async function getProducts() {
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
-      setProducts(data);
-    }
-
-    getProducts();
-  }, []);
-
-  useEffect(() => {
-    const newCart = products.map((product) => {
-      return { product: product, itemCount: 0 };
-    });
-    setCart(newCart);
-  }, [products]);
-
-  useEffect(() => {
-    const newCount = products.map((product) => {
-      return { id: product.id, count: 1 };
-    });
-    setInputCount(newCount);
-    console.log(newCount);
-  }, [products]);
-
-  function handleValue(id) {
-    const item = inputCount.find((product) => product.id === id);
-    if (!item) {
-      return 1;
-    }
-    return item.count;
-  }
-
-  function handleChange(evt) {
-    const newCount = inputCount.map((e) => {
-      if (e.id === parseInt(evt.target.name)) {
-        return {
-          id: parseInt(evt.target.name),
-          count: parseInt(evt.target.value),
-        };
-      }
-      return e;
-    });
-    console.log(newCount);
-    setInputCount(newCount);
-  }
-
-  function handleClick(evt) {
-    const newCart = cart.map(e => {
-      if (e.product.id === parseInt(evt.target.name)) {
-        const count = handleValue(e.product.id);
-        const newCount = e.itemCount + count
-        return { product: e.product, itemCount: newCount }
-      }
-      return e
-    })
-    setCart(newCart)
-    console.log(newCart)
-  }
+  const { getTotalCart } = useCart();
+  const totalItem = getTotalCart();
 
   return (
-    <div className="App">
-      <div className="products-container">
-        {products.map((e) => {
-          return (
-            <div key={e.id}>
-              <Product
-                title={e.title}
-                description={e.description}
-                image={e.image}
-                price={e.price}
-              />
-              <label htmlFor="quantity">Qty</label>
-              <input
-                type="number"
-                min={1}
-                value={handleValue(e.id)}
-                name={e.id}
-                onChange={handleChange}
-              ></input>
-              <button name={e.id} onClick={handleClick}>Add to cart</button>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+        </ul>
+        <Link to="/cart">
+          <button id="cart-btn">
+            <span id="cart-count">{totalItem}</span>
+          </button>
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/shop/:id" element={<ItemDetail />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </>
   );
 }
 
